cdec2fb627e10e05e07074443b2837e92ea5f588
Nikita <dangelotoff@mail.ru>
2026-04-09 16:57:16 +0400

    The logic for adding extensions to another database has been carefully designed

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -9,7 +9,8 @@ import (
 type DBRepository interface {
 	ExecuteQuery(ctx context.Context, query string) (string, error)
 	EnsureRoles(ctx context.Context, roles []string) error
-	EnsureExtensions(ctx context.Context, extensions []string) error
-	GetDatabaseInfo(ctx context.Context) (map[string][]formatter.DbObject, error)
+	EnsureExtensions(ctx context.Context, extensions []string, modifyTemplate bool) error
+	Analyze(ctx context.Context) error
+	GetSimpleClusterReport(ctx context.Context) (*formatter.ClusterSnapshot, error)
 	Close()
 }
